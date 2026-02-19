e09b4f75dcb4b4441694892df117eb0735feb59a
Nathan-Sasa <jonathanbikuta05@gmail.com>
2026-02-19 23:58:50 +0100

    feat: contact component

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
+import { FooterComponent } from '../../shared/layout/footer/footer.component';
+import { TextWriterDirective } from '../../shared/directives/textwriter.directive';
 
 @Component({
-  selector: 'app-contact.component',
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css',
+	selector: 'app-contact',
+	imports: [
+		CommonModule,
+		RouterModule,
+		FooterComponent,
+		TextWriterDirective
+	],
+	templateUrl: './contact.component.html',
+	styleUrl: './contact.component.css',
 })
 export class ContactComponent {
 
+	writer = [
+		'Contactez-nous au... +243 823-555-444 & 891-555-444',
+		'Et suivez-nous sur nos différents réseaux sociaux',
+		'Facebook',
+		'Instagram',
+		'Tiktok',
+		'Pinterest',
+		'',
+		'Commandez avant le 14 février pour recevoir 20% de reduction sur la couture de tenue de couple.'
+	]
 }
